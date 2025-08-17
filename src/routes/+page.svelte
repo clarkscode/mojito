bf1e1fd99544605cade6190cc247459998dc0053
clarkscode <jieclarkm@gmail.com>
2025-08-17 22:41:29 +0800

    Initial commit for mojito

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import gsap from 'gsap';
+	import { ScrollTrigger, SplitText } from 'gsap/all';
+
+	$effect(() => {
+		gsap.registerPlugin(ScrollTrigger, SplitText);
+	});
+</script>
+
+<div class="h-[100vh] flex-center">
+	<h1 class="text-3xl font-bold">Hello this is hero</h1>
+</div>
