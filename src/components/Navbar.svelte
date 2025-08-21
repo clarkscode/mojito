5efd507a919c3f509a4490f9cb13cf70c7ff8dee
clarkscode <jieclarkm@gmail.com>
2025-08-21 20:02:47 +0800

    initial hero section with gsap

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { navLinks } from '../constants';
+	import gsap from 'gsap';
+	$effect(() => {
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: 'nav',
+				start: 'bottom top'
+			}
+		});
+
+		navTween.fromTo(
+			'nav',
+			{
+				backgroundColor: 'transparent'
+			},
+			{
+				backgroundColor: '#00000050',
+				backgroundFilter: 'blur(10px)',
+				duration: 1,
+				ease: 'power1.inOut'
+			}
+		);
+	});
+</script>
+
+<nav>
+	<div>
+		<a href="#home" aria-label="home" class="flex items-center gap-2">
+			<img src="/images/logo.png" alt="Mojitos Logo" />
+			<p>Negroni</p>
+		</a>
+
+		<ul>
+			{#each navLinks as link (link.id)}
+				<li>
+					<a href={`#${link.id}`}>{link.title}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import gsap from 'gsap';
 	import { ScrollTrigger, SplitText } from 'gsap/all';
+	import Navbar from '../components/Navbar.svelte';
+	import Hero from '../components/Hero.svelte';
 
 	$effect(() => {
 		gsap.registerPlugin(ScrollTrigger, SplitText);
 	});
 </script>
 
-<div class="h-[100vh] flex-center">
-	<h1 class="text-3xl font-bold">Hello this is hero</h1>
-</div>
+<main>
+	<Navbar />
+	<Hero />
+	<div class="h-dvh bg-black"></div>
+</main>
